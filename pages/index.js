9f69bee05184835af0991170892a0bfc70831d8b
Umit Turk <uyt.turk@gmail.com>
2021-05-30 14:45:32 +0300

    build the header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -8,13 +9,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>Let build Hulu 2.0</h1>
+      {/* Header */}
+      <Header />
+      {/* Nav */}
 
-     {/* Header */}
-
-     {/* Nav */}
-
-     {/* Results */}
+      {/* Results */}
     </div>
-  )
+  );
 }

ecad7becdce33fd213c794b9375154238632bba7
Umit Turk <uyt.turk@gmail.com>
2021-05-30 22:09:39 +0300

    end of the project

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import {forwardRef} from "react";
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   console.log(result);
   return (
-    <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
+    <div ref={ref} className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
       <Image
         layout="responsive"
         src={
@@ -26,6 +27,6 @@ function Thumbnail({ result }) {
       </p>
     </div>
   );
-}
+})
 
 export default Thumbnail;

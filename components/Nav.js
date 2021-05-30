90d5b3811fa19864b5f0aa48b4b02080afa77d30
Umit Turk <uyt.turk@gmail.com>
2021-05-30 17:45:17 +0300

    navbar component

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,24 @@
+import request from "../utils/request";
+import {useRouter} from "next/router"
+
 function Nav() {
-    return (
-        <nav>
-            <div>
-                
-            </div>
-        </nav>
-    )
+    const router = useRouter();
+
+  return (
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide ">
+        {Object.entries(request).map(([key, { title }]) => (
+          <h2 key={key}
+          onClick={() => router.push(`/?genre=${key}`)}
+          className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] to_white h-10 w-1/12"/>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
